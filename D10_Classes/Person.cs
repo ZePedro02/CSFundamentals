70867bd67451b38643ee381bcd762e01707d4195
ze martins <zezepemarpe@hotmail.com>
2025-04-14 20:30:17 +0100

    1 parte aula

---
+++ D10_Classes/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D00_Utility;
+
+namespace D10_Classes
+{
+    internal class Person
+    {
+
+        #region Fields (variáveis privadas das propriedades)
+
+        private string name;
+
+        private string city;
+
+        #endregion
+
+        #region Propeties
+
+        #region Classic
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string City
+        {
+            get { return name; }
+            set { city = value; }
+        }
+
+        #endregion
+
+
+        #region Auto-implemented
+
+        public string VAT { get; set; }
+        public int ID { get; set; }
+        public string Phone { get; set; }
+        #endregion
+
+        #region Bodied-expression
+
+        #endregion
+
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+
+    }
+}
---
+++ D10_Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D00_Utility;
+namespace D10_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Utility.setUniCode();
+
+            #region instanciar a classe person (2 objetos)
+            Person person01 = new Person();
+            Person person02 = new Person();
+            #endregion
+
+            #region Atribur valores às propriedades
+            person01.Name = ("a");
+            person01.VAT = "123456789";
+            person02.Name = ("b");
+            person02.VAT = "987654321";
+            #endregion
+
+
+
+            Utility.terminateConsole();
+
+        }
+    }
+}
